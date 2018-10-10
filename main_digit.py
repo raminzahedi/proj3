638f1608983bf177b7bb114c76d20f023b595f41
raminzahedi <42061599+raminzahedi@users.noreply.github.com>
2018-10-10 02:01:52 -0600

    Create main_digit.py

---
+++ main_digit.py
@@ -0,0 +1,52 @@
+
+import time
+import warnings
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier 
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+
+def model_run(model, name):
+    start = time.time()
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        
+        model.fit(X_tr, Y_tr)
+        
+    end = time.time()
+    print('training time for', name, 'on digits dataset :', end - start, 'seconds')
+    
+    Y_pred = model.predict(X_ts)
+    error = (Y_ts != Y_pred).sum()
+    print('Misclassified samples: ', error)
+    print('Accuracy: ', metrics.accuracy_score(Y_ts, Y_pred), '\n')
+
+
+
+data, target = datasets.load_digits(return_X_y=True)
+X_tr, X_ts, Y_tr, Y_ts = train_test_split(data, target, test_size=0.3,
+                                          random_state=1, stratify=target)
+
+model = Perceptron()
+model_run(model, 'Perceptron')
+
+model = SGDClassifier(loss='hinge')
+model_run(model, 'Linear SVM')
+
+model = SVC(kernel='rbf')
+model_run(model, 'Non-linear SVM')
+
+model = DecisionTreeClassifier(criterion='gini')
+model_run(model, 'Decision Tree')
+
+model = KNeighborsClassifier(n_neighbors=5, metric='minkowski')
+model_run(model, 'KNN')
+
+model = LogisticRegression(C=100)
+model_run(model, 'Logistic Regression')
